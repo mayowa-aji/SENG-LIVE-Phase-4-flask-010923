ec48ef589669c2e2fa2fb7d82cc633a346dccf8f
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-03-14 13:14:21 -0400

    complete intro to flask

--- 01-intro-to-flask/server/app.py
+++ 01-intro-to-flask/server/app.py
@@ -7,22 +7,69 @@
 # 1. ✅ Navigate to `models.py`
 
 # 2. ✅ Set Up Imports
+from flask import Flask, jsonify, make_response, request
+from flask_migrate import Migrate
+
+from models import db, Production
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
 
 
 # 3. ✅ Initialize the App
-  
-    
+
+
     # Configure the database
     # ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'`
-    # ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False` 
-    
+    # ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False`
 
- # 4. ✅ Migrate 
+
+ # 4. ✅ Migrate
 
 # 5. ✅ Navigate to `seed.rb`
 
 # 6. ✅ Routes
-   
+
+@app.before_request
+def runs_before_request():
+    current_user={"user_id":1, "username":"rose"}
+    print(current_user)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World</h1>'
+
+# @app.route('/<str:image>')
+# def image():
+#     return f'<img src={image} alt={image.name} />'
+
+@app.route('/productions/<string:title>')
+def production(title):
+    # import ipdb; ipdb.set_trace()
+    production = Production.query.filter(Production.title == title).first()
+    production_response = {
+        "title": production.title,
+        "genre": production.genre,
+        "director": production.director,
+        "description": production.description,
+        "image" : production.image,
+        "budget": production.budget,
+        "ongoing": production.ongoing
+    }
+    response = make_response(
+        jsonify(production_response),
+        200
+    )
+    return response
+
+
+
+
+
 
 # 7. ✅ Run the server with `flask run` and verify your route in the browser at `http://localhost:5000/`
 
@@ -31,10 +78,15 @@
 
 
 # 9.✅ Update the route to find a `production` by its `title` and send it to our browser
-    
-   
 
-# Note: If you'd like to run the application as a script instead of using `flask run`, uncomment the line below 
+# @app.route('/context')
+# def context():
+#     import ipdb; ipdb.set_trace()
+
+
+
+
+# Note: If you'd like to run the application as a script instead of using `flask run`, uncomment the line below
 # and run `python app.py`
 
 # if __name__ == '__main__':
